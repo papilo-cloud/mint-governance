c639b1794d02091b2310ef026e41a926aa24d79b
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-19 16:03:30 +0100

    get the owner

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,8 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  const [owner] = await ethers.getSigners()
+
   const transactionCount = await owner.getTransactionCount();
 
   // gets the address of the token before it is deployed

bfb6563ab98e52b2331d8ced90322ba3bad610d5
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-19 23:55:55 +0100

    created a proposal

---
+++ scripts/proposal.js
@@ -0,0 +1,32 @@
+const { parseEther } = require('ethers/lib/utils');
+const {ethers} = require('hardhat')
+
+const governorAddress = '0xE2242660dD7F0265d7e8783528F78E21679D4e0c'
+const tokenAddress = '0x8D6076b24Cf40F4E519bd22Fb5B08E9F939f53e1';
+
+async function proposing() {
+    const [owner] = await ethers.getSigners()
+    const governor = await ethers.getContractAt('MyGovernor', governorAddress)
+    const token = await ethers.getContractAt('MyToken', tokenAddress)
+
+    const tx = await governor.propose(
+        [token.address],
+        [0],
+        [token.interface.encodeFunctionData('mint', [owner.address, parseEther('25000')])],
+        'Give the owner more tokens'
+    )
+
+    const receipt = await tx.wait();
+    const proposalId = receipt.events[0].args.proposalId;
+
+    console.log('Proposal created with Id:', proposalId.toString())
+
+    return {proposalId, governor};
+}
+
+proposing().catch(error => {
+    console.error(error);
+    process.exit(1);
+})
+
+module.exports = proposing;

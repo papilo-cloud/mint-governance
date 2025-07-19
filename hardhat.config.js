df3dff201e577b23ed3ba32064bda6da6598fef1
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-19 23:44:29 +0100

    add a etherscan private key to verify the contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+// require('@nomicfoundation/hardhat-verify');
 require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -9,5 +10,11 @@ module.exports = {
       url: process.env.SEPOLIA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: true
   }
 };
